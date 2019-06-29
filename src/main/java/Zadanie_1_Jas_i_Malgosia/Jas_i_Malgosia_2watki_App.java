0161edb301a0998386541a3bfb1096a37679202d
Dariusz.Żulpo <dd2000@wp.pl>
2019-06-29 14:03:31 +0200

    wielowątkowość + testy online i sprawdzian

--- src/main/java/Zadanie_1_Jas_i_Malgosia/Jas_i_Malgosia_2watki_App.java
+++ src/main/java/Zadanie_1_Jas_i_Malgosia/Jas_i_Malgosia_2watki_App.java
@@ -11,6 +11,9 @@ public class Jas_i_Malgosia_2watki_App implements Runnable{
 
         threadGosia.start();
 
+        threadGosia.join();  // join - czekaj aż wątek Gosia się zabije
+        threadJas.join();   // join - czekaj aż wątek Jaś się zabije
+        System.out.println("KONIEC DNIA !!! dla wszystkich");
     }
 
     public void run() {
